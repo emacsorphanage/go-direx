53f86604ba7c5d0fb583955bfba356ee4a8e5179
Syohei YOSHIDA <syohex@gmail.com>
2013-08-08 22:24:35 +0900

    add Comment section

--- go-direx.el
+++ go-direx.el
@@ -22,6 +22,16 @@
 
 ;;; Commentary:
 
+;; go-direx.el provides tree style source code viewer for Go langue
+;; using direx.
+;;
+;; To use this package, add these lines to your .emacs.d/init.el:
+;;     (require 'go-direx)
+;;
+;; I recommend you use go-direx with popwin.el. You can control popup
+;; buffer easily.
+;;
+
 ;;; Code:
 
 (eval-when-compile
